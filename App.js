404c7d503282d08c1384bc39c958e055f625acc7
Felipe de Paula <emilcyfelipe@gmail.com>
2021-11-23 18:35:24 -0300

    First Commit

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useRef, useState } from 'react';
+import { StyleSheet, Text, View, Animated} from 'react-native';
 
 export default function App() {
+  const [ bpmValue, setBpmValue ] = useState(80);
+ 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.display}>
+          <Text style={styles.valueText}>{bpmValue}</Text>
+          <Text style={[styles.valueText, {fontSize: 20}]}>Bpm</Text>
+          <Animated.View style={styles.markAxios}>
+            <View style={styles.mark}></View>
+          </Animated.View>
+      </View>
+      <View style={styles.actionsWrapper}></View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +23,45 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#1B1B1B',
     alignItems: 'center',
-    justifyContent: 'center',
+    
   },
+  display:{
+    width: '80%',
+    maxWidth: 800,
+    height: 200,
+    marginTop:50,
+    borderTopLeftRadius: 400,
+    borderTopRightRadius: 400,
+    borderColor: '#737373',
+    borderWidth: 10,
+    alignItems: 'center',
+  },
+  valueText:{
+    fontSize:70,
+    fontWeight: 'bold',
+    color: '#737373'
+  },
+  actionsWrapper:{
+    
+    backgroundColor: '#fff',
+    width: '100%'
+    
+  },
+  markAxios:{
+    width: '50%',
+    height: 10,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+    backgroundColor: '#fff',
+    zIndex:100
+  },
+  mark:{
+    width: 20,
+    height: 20,
+    borderRadius: 20/2,
+    backgroundColor: '#255255',
+  }
 });
